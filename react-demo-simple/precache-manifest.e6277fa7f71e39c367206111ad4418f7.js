1de1cbe90b79c092d3a8ae82d53846c1779c981d
Nguyen Ngoc Trong <nguyenngoctrong@Nguyen-Ngoc-Trong.local>
2019-12-06 16:05:33 +0700

    Create a react demo simple.

---
+++ react-demo-simple/precache-manifest.e6277fa7f71e39c367206111ad4418f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "511c67e0979d61d0e8ed178175de2002",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92d1f2f0eaef48e6edb6",
+    "url": "/static/css/main.3bb8359d.chunk.css"
+  },
+  {
+    "revision": "655c1ef83f69b767fc8b",
+    "url": "/static/js/2.b4d19de4.chunk.js"
+  },
+  {
+    "revision": "92d1f2f0eaef48e6edb6",
+    "url": "/static/js/main.f26078bf.chunk.js"
+  },
+  {
+    "revision": "2bd63f29f0b0b7537a05",
+    "url": "/static/js/runtime-main.1d18de3b.js"
+  }
+]);
