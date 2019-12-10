79aa3bc89a112ade14e210923ce7ef1611b59b47
Nguyen Ngoc Trong <nguyenngoctrong@Nguyen-Ngoc-Trong.local>
2019-12-10 18:42:52 +0700

    Add demo react-demo-todolist/.

---
+++ react-demo-todolist/precache-manifest.e4e556dd440c53fceb1f4b9a287ce84c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11a3cad65316cb5db9b458772276076e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "771d275c318fcd02d61b",
+    "url": "/static/css/main.f399a50b.chunk.css"
+  },
+  {
+    "revision": "c3810f11b31bfc759e05",
+    "url": "/static/js/2.c4c85954.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.c4c85954.chunk.js.LICENSE"
+  },
+  {
+    "revision": "771d275c318fcd02d61b",
+    "url": "/static/js/main.ff40de48.chunk.js"
+  },
+  {
+    "revision": "f0642754e3a4fb711f35",
+    "url": "/static/js/runtime-main.3201fede.js"
+  }
+]);
